423223c7c005308d937b6172cc645efb0a60f670
jaherlihy <jaherlihy@gmail.com>
2021-08-16 18:40:11 -0500

    just getting the repo setup

---
+++ script.js
@@ -0,0 +1,23 @@
+var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+
+
+
+var foo = [];
+var foo = array.prototype.fill()
+
+foo.length = 26;
+
+foo.fill("A");
+
+
+for (var i = 0; i < howeverLong; i++) {
+	//pick something random
+	foo.push(someRandomData);
+}
